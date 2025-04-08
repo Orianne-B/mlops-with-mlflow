2832f019cc2ad2fed7f555ff2bedcf1e41be772e
Orianne B <orianne.laura.bargain@gmail.com>
2025-04-08 17:26:02 +0200

    use normal autolog

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -8,7 +8,7 @@ from sklearn.ensemble import RandomForestClassifier
 root_folder = pathlib.Path("__file__").resolve().parent
 
 mlflow.set_tracking_uri(uri="http://127.0.0.1:8080")
-mlflow.sklearn.autolog()
+mlflow.autolog()
 
 
 def train_model(
