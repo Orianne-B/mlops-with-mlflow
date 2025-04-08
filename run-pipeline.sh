1e20c8fdaa0155a07482acadce003897198a31c9
Orianne B <orianne.laura.bargain@gmail.com>
2025-04-08 15:33:55 +0200

    fix: relativ path

--- run-pipeline.sh
+++ run-pipeline.sh
@@ -3,6 +3,9 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+# Get the directory of the current script
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+
 # Check if the required arguments are provided
 if [ "$#" -lt 1 ]; then
   echo "Usage: $0 <input_data> <processed_data_folder> <model_folder>"
@@ -11,14 +14,14 @@ fi
 
 # Step 1: Prepare the data
 echo "Preparing data..."
-python3 /home/administrateur/exo/mlops-simple/scripts/prepare_data.py  --input_data "$1" --output_folder "$2"
+python3 "$SCRIPT_DIR/scripts/prepare_data.py"  --input_data "$1" --output_folder "$2"
 
 # Step 2: Train the model
 echo "Training the model..."
-python3 /home/administrateur/exo/mlops-simple/scripts/train_model.py  --model_folder "$3"
+python3 "$SCRIPT_DIR/scripts/train_model.py"  --model_folder "$3"
 
 # Step 3: Evaluate the model
 echo "Evaluating the model..."
-python3 /home/administrateur/exo/mlops-simple/scripts/evaluate_model.py  --model_folder "$3" --data_folder "$2"
+python3 "$SCRIPT_DIR/scripts/evaluate_model.py"  --model_folder "$3" --data_folder "$2"
 
 echo "Pipeline execution completed successfully."
