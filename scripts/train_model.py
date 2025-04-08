75718d74d4e5d31689f4b2c42815433daad38354
Orianne B <orianne.laura.bargain@gmail.com>
2025-04-08 16:42:40 +0200

    fix: remove exp name

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -8,7 +8,6 @@ from sklearn.ensemble import RandomForestClassifier
 root_folder = pathlib.Path("__file__").resolve().parent
 
 mlflow.set_tracking_uri(uri="http://127.0.0.1:8080")
-mlflow.set_experiment(experiment_name="ML Ops with ML Flow")
 mlflow.sklearn.autolog()
 
 
